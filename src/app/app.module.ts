fc8bf6d187a974dd7f18135a4697bc009df7e798
ketsarin <ketsarin.sontisuk@gmail.com>
2020-04-29 21:51:03 +0200

    Removed extra login folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { LoginComponent } from './login/login/login.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
