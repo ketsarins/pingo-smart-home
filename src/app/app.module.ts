9cfc5b8d61f194d37954fd52954c02cb15efa779
ketsarin <ketsarin.sontisuk@gmail.com>
2020-04-29 22:06:34 +0200

    Generated material module and moved out from import in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
+
 import { LoginComponent } from './login/login.component';
+import { CoreMaterialModule } from './core/material.module';
 
 @NgModule({
   declarations: [
@@ -18,9 +17,7 @@ import { LoginComponent } from './login/login.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatCardModule,
-    MatInputModule,
-    MatButtonModule
+    CoreMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
